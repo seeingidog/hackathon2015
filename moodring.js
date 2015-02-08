5aceb4bd3d353a06c5367a397fc7fcdb07e95c43
Danny Leavitt <danny.leavitt@gmail.com>
2015-02-07 17:14:22 -0800

    first commit

--- moodring.js
+++ moodring.js
@@ -1,7 +1,18 @@
 // Mood Ring Code
 
 $(document).ready(function() {
-    $("._42fz").each(function() {
+   /* $("._42fz").each(function() {
         $(this).append('<div class="moodlet"></div>');
-    });
+    });*/
+	var userIds = ["1402854887", "100000464907991","56456456546"];
+	$.each(userIds, function( index, theUserId ) {
+		//check if user is present in chat messenger
+		console.log(theUserId);
+		theSelector = "li[data-id='" + theUserId + "']" + "> a > div > div._5bon";
+		console.log(theSelector);
+		response = $(theSelector).prepend('<span style="width:5px; padding-right:4px">tt</span>');
+		console.log(response);
+		//$("[]=").append();
+	});
+	
 });
